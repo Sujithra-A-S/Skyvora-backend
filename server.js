7748ae7649e4f55f2038250a134da4ca8580f40b
Sujithra-A-S <sujithraas04@gmail.com>
2026-07-02 23:36:06 +0530

    use gmailsmtp 587

--- server.js
+++ server.js
@@ -230,7 +230,11 @@ const enquirySchema = new mongoose.Schema({
 const Enquiry = mongoose.model("Enquiry", enquirySchema);
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port:587,
+  secure: false,
+  requireTLS:true,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
